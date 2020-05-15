3859437cf49d7b4fe268f293c0d8336590434b5c
Krzysztof Kalamarski <k.kalamarski@exlabs.pl>
2020-05-15 13:24:40 -0300

    Rename Scrolls -> Gems

--- src/main/java/net/itv/portals/ItvPortals.java
+++ src/main/java/net/itv/portals/ItvPortals.java
@@ -1,11 +1,14 @@
 package net.itv.portals;
 
+import express.Express;
 import net.fabricmc.api.ModInitializer;
 import net.itv.portals.items.WarpScroll;
+import net.minecraft.command.arguments.NbtPathArgumentType.NbtPath;
+import net.minecraft.datafixer.NbtOps;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class ItvPortals implements ModInitializer {
 
@@ -14,5 +17,11 @@ public class ItvPortals implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("itv-portals", "warp_scroll"), WARP_SCROLL);
+
+		Express app = new Express();
+
+		app.get("/", (req, res) -> {
+			res.send("Hello World");
+		}).listen();
 	}
 }
--- src/main/java/net/itv/portals/items/WarpScroll.java
+++ src/main/java/net/itv/portals/items/WarpScroll.java
@@ -1,21 +1,48 @@
 package net.itv.portals.items;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Executor;
 
+import com.mojang.datafixers.DataFixer;
+import com.mojang.datafixers.DataFixerBuilder;
+import com.mojang.realmsclient.dto.PlayerInfo;
+
+import org.apache.logging.log4j.core.jmx.Server;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.fabricmc.loader.util.sat4j.minisat.core.DataStructureFactory;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.data.dev.NbtProvider;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.IntArrayTag;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.TagReaders;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.NbtText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.DimensionType;
 
 public class WarpScroll extends Item {
 
@@ -25,15 +52,18 @@ public class WarpScroll extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
-        player.playSound(SoundEvents.BLOCK_PORTAL_TRAVEL, 1.0F, 1.0F);
-
-        BlockPos spawn = player.getSpawnPosition();
-
-        player.setPos(spawn.getX(), spawn.getY(), spawn.getZ());
-        player.positAfterTeleport(spawn.getX(), spawn.getY(), spawn.getZ());
-
         ItemStack item = player.getStackInHand(hand);
+
         item.decrement(1);
+
+        if (world.isClient()) {
+            player.playSound(SoundEvents.BLOCK_PORTAL_TRAVEL, 1.0F, 1.0F);
+        } else {
+            BlockPos spawn = ((ServerPlayerEntity) player).getSpawnPointPosition();
+
+            player.setPos(spawn.getX(), spawn.getY(), spawn.getZ());
+            player.positAfterTeleport(spawn.getX(), spawn.getY(), spawn.getZ());
+        }
 
         return new TypedActionResult<ItemStack>(ActionResult.SUCCESS, item);
     }
